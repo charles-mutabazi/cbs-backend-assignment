8e788ecd5cf95989fbacdf34a0ca360127bf5648
Charles Mutabazi <charles.mutabazi@oneacrefund.org>
2024-08-21 13:28:14 +0200

    set up containarization of the app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => {
+    console.log(
+      `Server is running on http://localhost:${PORT} in ${process.env.NODE_ENV} mode`,
+    );
+  });
 }
 bootstrap();
